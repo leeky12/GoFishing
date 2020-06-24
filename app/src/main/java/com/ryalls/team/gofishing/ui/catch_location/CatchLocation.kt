f46e4a61a4b69f3e29f3a2275163c8f90ea44676
Leeky <lee.comp@ntlworld.com>
2020-06-24 11:15:04 +0100

    Basic of location map working

--- app/src/main/java/com/ryalls/team/gofishing/ui/catch_location/CatchLocation.kt
+++ app/src/main/java/com/ryalls/team/gofishing/ui/catch_location/CatchLocation.kt
@@ -5,38 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.GoogleMapOptions
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.ryalls.team.gofishing.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import kotlinx.android.synthetic.main.app_bar_start_activity.*
 
 /**
  * A simple [Fragment] subclass.
  * Use the [CatchLocation.newInstance] factory method to
  * create an instance of this fragment.
  */
-class CatchLocation : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
+class CatchLocation : Fragment(), OnMapReadyCallback {
+
     private var param2: String? = null
+    private lateinit var mMap: GoogleMap
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.catch_location, container, false)
+        val root = inflater.inflate(R.layout.catch_location, container, false)
+        val fm = childFragmentManager
+
+        // set up the initial location, layout etc. before the map appears
+        val options = GoogleMapOptions()
+            .zoomControlsEnabled(true)
+            .compassEnabled(true)
+            .mapType(GoogleMap.MAP_TYPE_HYBRID)
+
+        val fragment = SupportMapFragment.newInstance(options)
+        fm.beginTransaction().replace(R.id.locationmap, fragment).commit()
+        fragment.getMapAsync(this)
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        activity?.fab?.hide()
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
     }
 
     companion object {
@@ -51,11 +64,6 @@ class CatchLocation : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            CatchLocation().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+            CatchLocation()
     }
 }
