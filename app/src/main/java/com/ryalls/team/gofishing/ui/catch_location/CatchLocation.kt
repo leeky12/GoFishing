f3a70b251f744a65098e746ae10ba76831b3286c
Leeky <lee.comp@ntlworld.com>
2020-06-24 10:50:19 +0100

    Adding new location view

--- app/src/main/java/com/ryalls/team/gofishing/ui/catch_location/CatchLocation.kt
+++ app/src/main/java/com/ryalls/team/gofishing/ui/catch_location/CatchLocation.kt
@@ -1,4 +1,4 @@
-package com.ryalls.team.gofishing.ui.identify_fish
+package com.ryalls.team.gofishing.ui.catch_location
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ryalls.team.gofishing.R
-import kotlinx.android.synthetic.main.app_bar_start_activity.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,13 +14,14 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [identify.newInstance] factory method to
+ * Use the [CatchLocation.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Identify : Fragment() {
+class CatchLocation : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,17 +31,12 @@ class Identify : Fragment() {
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        activity?.fab?.hide()
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.catch_identify, container, false)
+        return inflater.inflate(R.layout.catch_location, container, false)
     }
 
     companion object {
@@ -51,12 +46,12 @@ class Identify : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment settings.
+         * @return A new instance of fragment BlankFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            Identify().apply {
+            CatchLocation().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
